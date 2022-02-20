b061753f7e2d95405976e39014b4459da3670811
2001adarsh <2001adarshsingh@gmail.com>
2022-02-20 23:13:44 +0530

    made confidential changes

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCB6Xxn_vAVAMqHdf6fiY7tK9SYeQco1w0",
-  authDomain: "twitter-clone-bd877.firebaseapp.com",
-  projectId: "twitter-clone-bd877",
-  storageBucket: "twitter-clone-bd877.appspot.com",
-  messagingSenderId: "895292992662",
-  appId: "1:895292992662:web:7a644048628b80972426b7",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
